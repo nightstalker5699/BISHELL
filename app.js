9bee13560393d1a2aa202599fcbdff77d2ee3318
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2024-10-23 22:43:24 +0300

    added cookie and cors settings

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const morgan = require('morgan');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const express = require("express");
+const morgan = require("morgan");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const app = express();
-const fs = require('fs');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
-const hpp = require('hpp');
-
+const fs = require("fs");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+app.enable("trust proxy");
 
 // Global middlewares
+app.use(cors());
+
+app.options("*", cors());
 
 app.use(helmet());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 const limiter = rateLimit({
@@ -25,20 +30,23 @@ const limiter = rateLimit({
   message: "Too many requests from this IP, please try again in an hour!",
 });
 
-app.use('api', limiter);
+app.use("api", limiter);
 
-
-app.use(express.json({
-  limit: '10kb'
-})); // Middleware to parse the body of the request
-
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+); // Middleware to parse the body of the request
+app.use(cookieParser());
 app.use(mongoSanitize()); // Middleware to sanitize the input data
 
 app.use(xss()); // Middleware to prevent XSS attacks
 
-app.use(hpp({
-  whitelist: []
-})); // Middleware to prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+); // Middleware to prevent parameter pollution
 
 app.use(express.static(`${__dirname}/public`)); // Middleware to serve static files
 app.use((req, res, next) => {
@@ -49,8 +57,7 @@ app.use((req, res, next) => {
 //Middleware mounting
 //app.use(`/api/v1/users`, userRouter);
 
-
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
