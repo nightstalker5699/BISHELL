5c3cc87b31172157eddc81e457ea6d8b9a237289
Nouralddin <noorabdo5577@gmail.com>
2024-10-25 20:53:55 +0300

    username, getone adjust
    zp : token
    
    usernmae unique only characters and digits
    getOne by username now instead of id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,13 @@
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
-const factory = require("./handlerFactory")
+const factory = require("./handlerFactory");
 
-exports.getAllUsers = factory.getAll(User)
+exports.getAllUsers = factory.getAll(User);
 
-exports.getMe = (req,res,next) => {
-    req.params.id = req.user.id
-    next()
-  }
+exports.getMe = (req, res, next) => {
+  req.params.username = req.user.username;
+  next();
+};
 
-  exports.getUser = factory.getOne(User)
-  
+exports.getUser = factory.getOne(User);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,13 +12,13 @@ router.get(
   userController.getMe,
   userController.getUser
 );
-
+router.route('/:username').get(authController.protect, userController.getUser);
 router.patch("/updateMyPassword",authController.protect, authController.updatePassword);
 
 router.post(`/signup`, authController.signup);
 router.post(`/login`, authController.login);
 router.route(`/forgotPassword`).post(authController.forgotPassword);
-router.route(`/resetPassword/:zp`).patch(authController.resetPassword);
+router.route(`/resetPassword/:token`).patch(authController.resetPassword);
 
 router.route(`/`).get(authController.protect,userController.getAllUsers);
 
