c92d141f7b7ebcf9064fa56ac63664f958549eb6
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-10-28 21:10:09 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,20 +10,6 @@ exports.getMe = (req, res, next) => {
   next();
 };
 
-exports.getUser = factory.getOne(User);
-
-const User = require("../models/userModel");
-const AppError = require("../utils/appError");
-const catchAsync = require("../utils/catchAsync");
-const factory = require("./handlerFactory");
-
-exports.getAllUsers = factory.getAll(User);
-
-exports.getMe = (req, res, next) => {
-  req.params.username = req.user.username;
-  next();
-};
-
 exports.getUser = catchAsync(async (req, res, next) => {
   const targetUser = await User.findOne({ username: req.params.username })
     .select("-passwordResetToken -passwordResetTokenExpires -passwordChangedAt");
