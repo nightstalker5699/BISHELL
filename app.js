35f02b426ae871833be4339289ed4bf0d5c3118c
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-11-04 20:52:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,14 +16,12 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const xss = require("xss-clean");
 const mongoSanitize = require("express-mongo-sanitize");
-// const hpp = require("hpp");
+const hpp = require("hpp");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
-
 app.enable("trust proxy");
 const multiPartParser = multer();
-
 // Global middlewares
 app.use(cors({
   origin: '*',
@@ -43,6 +41,10 @@ app.use(helmet({
   },
 }));
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 const limiter = rateLimit({
   max: 1000,
   windowMs: 60 * 60 * 1000,
@@ -54,26 +56,26 @@ app.use("/api", limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(multiPartParser.any()); // To parse multipart/form-data
+app.use(multiPartParser.any()); // to parse multipart/form data
+// Middleware to parse the body of the request
 app.use(cookieParser());
-app.use(mongoSanitize()); // Sanitize input data
+app.use(mongoSanitize()); // Middleware to sanitize the input data
 
-// app.use(xss()); // Prevent XSS attacks
+app.use(xss()); // Middleware to prevent XSS attacks
 
-// app.use(
-//   hpp({
-//     whitelist: [],
-//   })
-// ); // Prevent HTTP parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+); // Middleware to prevent parameter pollution
 
-app.use(express.static(path.join(__dirname, "static"))); // Serve static files
-
+app.use(express.static(path.join(__dirname, "static"))); // Middleware to serve static files
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Route mounting
+//Middleware mounting
 app.use(`/api/users`, userRouter);
 app.use(`/api/points`, pointRouter);
 app.use(`/api/courses`, courseRouter);
