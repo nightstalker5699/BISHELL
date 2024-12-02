289f1ccaeb84f12a7b820a57386b09e7829b47ad
Nouralddin <43034992+nouralddin-abdullah@users.noreply.github.com>
2024-12-02 19:14:08 +0200

    mongodb manual days sorting + hours
    mongodb doesn't have automatic days - hours sorting as function on it

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -10,11 +10,9 @@ exports.getAllSchedule = factory.getAll(Schedule);
 exports.getSchedule = catchAsync(async (req, res, next) => {
   const groupLetter = req.params.groupLetter;
   const baseQuery = Schedule.find({ group: groupLetter });
-  
-  const features = new apiFeatures(baseQuery, { sort: 'day,startTime' })
-    .sort();
 
-  const schedules = await features.query.populate({
+  
+  const schedules = await baseQuery.populate({
     path: 'courseId',
     select: 'courseName instructorName'
   });
@@ -23,10 +21,40 @@ exports.getSchedule = catchAsync(async (req, res, next) => {
     return next(new AppError('No schedules found for this group', 404));
   }
 
+  
+  const dayOrder = {
+    'Saturday': 1,
+    'Sunday': 2,
+    'Monday': 3,
+    'Tuesday': 4,
+    'Wednesday': 5,
+    'Thursday': 6,
+    'Friday': 7
+  };
+
+  
+  const convertTo24Hour = (time) => {
+    const [hour, period] = time.split(' ');
+    let hourNum = parseInt(hour);
+    if (period.toUpperCase() === 'PM' && hourNum !== 12) hourNum += 12;
+    if (period.toUpperCase() === 'AM' && hourNum === 12) hourNum = 0;
+    return hourNum;
+  };
+
+  
+  const sortedSchedules = schedules.sort((a, b) => {
+    
+    const dayDiff = dayOrder[a.day] - dayOrder[b.day];
+    if (dayDiff !== 0) return dayDiff;
+    
+    
+    return convertTo24Hour(a.startTime) - convertTo24Hour(b.startTime);
+  });
+
   res.status(200).json({
     status: 'success',
     data: {
-      schedules
+      schedules: sortedSchedules
     }
   });
 });
