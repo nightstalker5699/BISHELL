77791d574ba26308d984a594224c7eea4fa09aca
nightstalker5699 <94228182+nightstalker5699@users.noreply.github.com>
2024-12-24 02:33:00 +0200

    debug

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -25,7 +25,7 @@ const handleJWTExpiredError = () =>
   new AppError("your token has expired. please log in again", 401);
 const sendErrorDev = (err, req, res) => {
   // API //
-
+  console.log(err);
   res.status(err.statusCode).json({
     status: err.status,
     error: err,
